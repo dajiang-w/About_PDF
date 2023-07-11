1251f442aa84d1363ccba7489c2d655bf3e6e19d
Dajiang Wang <wang_dajiang@hotmail.com>
2023-07-11 09:18:56 +0800

    version_1.2

--- README.md
+++ README.md
@@ -12,3 +12,17 @@ codes about PDF processing
   PyPDF 3.0.1
   
   pdfplumber 0.6.2
+
+
+# 用法
+## pdf批量转图片
+```cmd
+python pdfs2imgs.py F:\pdfs F:\imgs
+```
+F:\pdfs是pdf所在的目录, F:\imgs是图片文件夹
+
+## 图片转pdf
+```cmd
+python imgs2pdf.py F:\imgs
+```
+F:\imgs是图片文件夹，转换完成后，会在F:\imgs生成一个以当前时间为文件名的pdf文件。
--- pdfs2imgs.py
+++ pdfs2imgs.py
@@ -10,14 +10,15 @@ def pdf2pic(pdfspath,imgspath,format,resolution): # format:bmp,png,jpeg,tiff
     index = 1
     for pdffile in pdffiles:
         pdfpath = os.path.join(pdfspath, pdffile)
-        pdf = pp.open(pdfpath)  # 打开pdf
-        pages =  pdf.pages  # 获取指定页面
-        for page in pages:
-            pgimg = page.to_image(resolution = resolution)
-            imagefile = os.path.join(imgspath,"Image" + str(index) + "." + format)
-            print("\r converting " + imagefile)
-            pgimg.save(imagefile,format = format) # PNG无损，也可以用其它格式如tiff,jpeg
-            index += 1
+        if pdfpath.endswith('.pdf'):
+            pdf = pp.open(pdfpath)  # 打开pdf
+            pages =  pdf.pages  # 获取指定页面
+            for page in pages:
+                pgimg = page.to_image(resolution = resolution)
+                imagefile = os.path.join(imgspath,"Image" + str(index) + "." + format)
+                print("\r converting " + imagefile)
+                pgimg.save(imagefile,format = format) # PNG无损，也可以用其它格式如tiff,jpeg
+                index += 1
 
 
 if __name__ == "__main__":
